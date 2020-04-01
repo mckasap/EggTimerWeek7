25365af45385db250b952b55ff3c751d3604be1c
mckasap <mckasap@gmail.com>
2020-04-02 01:50:17 +0300

    Sileder kodu eklendi Dakika Düzeltildi

--- app/src/main/java/org/kasapbasi/eggtimerweek7/MainActivity.java
+++ app/src/main/java/org/kasapbasi/eggtimerweek7/MainActivity.java
@@ -3,12 +3,45 @@ package org.kasapbasi.eggtimerweek7;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+SeekBar sb;
+TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sb=(SeekBar)findViewById(R.id.timerSeekBar);
+        sb.setMax(600); //10 dakika için
+        sb.setMin(1);
+        tv= (TextView)findViewById(R.id.tvTimer);
+
+
+
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                //ilk önce saniyeden dakika ve saniyeye dönüştürmek
+
+                int minute;
+                int seconds;
+                minute=(int) progress/60;
+                seconds= progress-60*minute;
+                tv.setText(Integer.toString(minute)+":"+Integer.toString(seconds));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
