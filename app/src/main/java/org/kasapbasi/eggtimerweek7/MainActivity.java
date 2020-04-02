b0b5bbde1c84ec91691fb8b5cf2d5425b3f8de93
mckasap <mckasap@gmail.com>
2020-04-02 16:16:20 +0300

    Mdifiye DErs Seekbar değişim

--- app/src/main/java/org/kasapbasi/eggtimerweek7/MainActivity.java
+++ app/src/main/java/org/kasapbasi/eggtimerweek7/MainActivity.java
@@ -2,7 +2,9 @@ package org.kasapbasi.eggtimerweek7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.*;
 import android.widget.TextView;
@@ -13,8 +15,27 @@ public class MainActivity extends AppCompatActivity {
 SeekBar sb;
 TextView tv;
 Button btn;
+CountDownTimer cdt;
+
 
 public void tiklaBeni(View v){
+
+cdt = new CountDownTimer(sb.getProgress()*1000,1000) {
+    @Override
+    public void onTick(long millisUntilFinished) {
+
+        timerUpdate((int) millisUntilFinished/1000);
+    }
+
+    @Override
+    public void onFinish() {
+        MediaPlayer mp= MediaPlayer.create(MainActivity.this,R.raw.horn);
+        mp.start();
+
+    }
+};
+
+cdt.start();
 
 }
 
@@ -32,14 +53,7 @@ public void tiklaBeni(View v){
         sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                int minute;
-                int second;
-                minute =(int) progress/60;
-                second = progress-60*minute;
-
-                tv.setText(minute+":"+second);
-
-
+                timerUpdate(progress);
             }
 
             @Override
@@ -56,5 +70,14 @@ public void tiklaBeni(View v){
 
 
 
+    }
+
+    private void timerUpdate(int progress) {
+        int minute;
+        int second;
+        minute = progress /60;
+        second = progress-60*minute;
+
+        tv.setText(minute+":"+second);
     }
 }
